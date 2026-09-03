53c23de8d289b8549ab61b55682a23aaeddedf78
Sumit <satputesumit005@gmail.com>
2026-09-03 16:35:41 +0530

    Allow Render host

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = os.environ.get(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "jobportalbackend-uypb.onrender.com",
+]
 
 
 # Application definition
